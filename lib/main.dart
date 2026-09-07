9c4a2266c35e0656fa5d5eb43bebea45832d0232
Bzi6 <92222u@gmail.com>
2026-09-07 17:39:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/main_screen.dart';
 
 void main() {
-  runApp(const BaiyunKeysApp());
+  runApp(const MyApp());
 }
 
-class BaiyunKeysApp extends StatelessWidget {
-  const BaiyunKeysApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '包子的key',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF10B981)),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF5F7FA),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Color(0xFF111827),
+          elevation: 0,
+          centerTitle: true,
+        ),
+      ),
+      home: const MainScreen(),
     );
   }
 }
